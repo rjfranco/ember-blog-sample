509789e1c768b740e46dee045671fcf5309f5f2b
Ramiro Jr. Franco <me@ramiro.mx>
2019-03-30 18:21:38 -0700

    sorting blogs by date on index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
+import { sort } from '@ember/object/computed';
 
 export default Controller.extend({
+  blogs: sort('model', (a, b) => {
+    if (a.date > b.date) return -1;
+    if (a.date < b.date) return 1;
+    return 0;
+  }),
+
   actions: {
     goToBlog(blog) {
       this.transitionToRoute('blog', blog);
