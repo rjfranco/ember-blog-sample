40e67a33003bfe78dd4ed37ea0f78fc969204f8f
Ramiro Jr. Franco <me@ramiro.mx>
2019-03-30 19:37:12 -0700

    Adding model test for display dates

--- tests/unit/models/blog-test.js
+++ tests/unit/models/blog-test.js
@@ -5,9 +5,11 @@ module('Unit | Model | blog', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('gives a friendly display time', function(assert) {
     let store = this.owner.lookup('service:store');
-    let model = store.createRecord('blog', {});
-    assert.ok(model);
+    let model = store.createRecord('blog', {
+      date: new Date('1995-12-17T03:24:00')
+    });
+    assert.ok(model.get('displayDate').indexOf('years ago') > -1);
   });
 });
