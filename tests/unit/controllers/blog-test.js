4b87d0a4f4a7d6a020d50851f4784f8d8d57c694
Ramiro Jr. Franco <me@ramiro.mx>
2019-03-30 19:27:10 -0700

    setup paragraphs display test

--- tests/unit/controllers/blog-test.js
+++ tests/unit/controllers/blog-test.js
@@ -5,8 +5,13 @@ module('Unit | Controller | blog', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it can display individual paragraphs from generated content', function(assert) {
     let controller = this.owner.lookup('controller:blog');
-    assert.ok(controller);
+    controller.set('model', {
+      content: `Voluptas qui nesciunt modi qui voluptatum. Et nostrum illo. Tempora qui at tempore dolorem. Deserunt sed nulla saepe. Natus eligendi ut voluptatibus voluptas totam ut recusandae quaerat rem. Reiciendis quis enim ipsum dignissimos ab beatae fuga corrupti quaerat.
+Fugit laudantium maiores. Tenetur ipsa inventore repellendus perspiciatis. Inventore nulla ut sint non quia nesciunt et aperiam. Inventore vero aperiam et quis est odio culpa et.
+Ut iure repellendus. Iure necessitatibus eos quia eum dolores. Qui molestiae molestiae incidunt quia qui pariatur eaque libero. Labore doloremque laborum nihil debitis quae at laboriosam corrupti optio. Consequatur consequatur exercitationem eum.`
+    })
+    assert.equal(controller.get('paragraphs.length'), 3);
   });
 });
