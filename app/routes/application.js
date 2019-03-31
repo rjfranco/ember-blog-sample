b7317118160c47232414ba2eed80a27e0c1e57a5
Ramiro Jr. Franco <me@ramiro.mx>
2019-03-30 18:21:29 -0700

    Adding indexedb cached blog dates to preverse refresh functionality on blog details

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,25 +1,60 @@
 import Route from '@ember/routing/route';
 import faker from 'faker';
+import { openDB } from 'idb';
 
 export default Route.extend({
   model() {
-    this.setupBlogRecords();
+    return this.setupBlogRecords();
   },
 
-  generateBlogRecords() {
+  addEntriesToStore(entries) {
+    for (let i=0; i < entries.length; i++) {
+      this.store.createRecord('blog', entries[i]);
+    }
+  },
+
+  generateBlogEntries(db) {
+    let entries = [];
+    const transaction = db.transaction('blogs', 'readwrite');
+
     for (let i=0; i<100; i++) {
-      this.store.createRecord('blog', {
+      let newEntry = {
         author: faker.name.findName(),
         avatar: faker.image.avatar(),
         content: faker.lorem.paragraphs(),
         date: faker.date.past(),
         id: faker.random.uuid(),
         title: faker.random.words()
-      });
+      }
+      entries.push(newEntry);
+      transaction.store.add(newEntry);
     }
+
+    return entries;
   },
 
   setupBlogRecords() {
-    this.generateBlogRecords();
+    return new Promise((resolve) => {
+      this.setupDB().then((db) => {
+        const dbRequestPromise = db.getAllFromIndex('blogs', 'id');
+        return dbRequestPromise.then((entries) => {
+          if (!entries.length) {
+            entries = this.generateBlogEntries(db);
+          }
+
+          this.addEntriesToStore(entries);
+          resolve();
+        });
+      });
+    });
+  },
+
+  setupDB() {
+    return openDB('el-blog', 1, {
+      upgrade(db) {
+        const store = db.createObjectStore('blogs', { keyPath: 'id' });
+        store.createIndex('id', 'id');
+      }
+    });
   }
 });
