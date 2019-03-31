bfe29eb3527cc8fe1835fe67a44d046ac9f68fad
Ramiro Jr. Franco <me@ramiro.mx>
2019-03-30 19:43:18 -0700

    Adding test to ensure index can sort its blogs

--- tests/unit/controllers/index-test.js
+++ tests/unit/controllers/index-test.js
@@ -5,8 +5,28 @@ module('Unit | Controller | index', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('can sort it\'s models', function(assert) {
     let controller = this.owner.lookup('controller:index');
-    assert.ok(controller);
+
+    let model = [
+      {
+        title: 'b',
+        date: new Date('2000-01-02T00:00:00')
+      },
+      {
+        title: 'c',
+        date: new Date('2000-01-01T00:00:00')
+      },
+      {
+        title: 'a',
+        date: new Date('2000-01-03T00:00:00')
+      }
+    ];
+
+    controller.set('model', model);
+
+    let blogsOrder = controller.get('blogs').map((model) => { return model.title }).join('');
+
+    assert.equal(blogsOrder, 'abc')
   });
 });
